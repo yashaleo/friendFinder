a289ba70b96fb22a235d9d7ddfe34012b6442729
yashaleo <yashaleo@me.com>
2019-07-21 17:09:45 -0500

    created files and folders, added server info, got html information from exammple, added new links for repo and image

---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Sets up Routes
+// =============================================================
+// require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+// require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
